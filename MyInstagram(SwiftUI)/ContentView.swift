8fcf277a5db569f9663fadb9b7a4b1b2a571787d
purnami <purnami.indryaswari@gmail.com>
2024-11-10 14:13:28 +0800

    Initial Commit

---
+++ MyInstagram(SwiftUI)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyInstagram(SwiftUI)
+//
+//  Created by purnami indryaswari on 10/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyInstagram(SwiftUI)/MyInstagram_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  MyInstagram_SwiftUI_App.swift
+//  MyInstagram(SwiftUI)
+//
+//  Created by purnami indryaswari on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MyInstagram_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
